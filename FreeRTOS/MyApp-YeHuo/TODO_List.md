bf95b6c1858a518b7ace78888566d47e4d33fad6
csy <csydlut@163.com>
2021-03-19 11:02:48 +0800

    Change some folder name.

---
+++ FreeRTOS/MyApp-YeHuo/TODO_List.md
@@ -0,0 +1,12 @@
+# Project Template
+
+Change Template to ABOLO STM32F767 Board.
+
+Find out its BSP folder.
+
+
+#
+
+#
+
+#
--- FreeRTOS/MyOwnOS/Code/Project/Objects/list.d
+++
@@ -1,6 +0,0 @@
-.\objects\list.o: ..\FreeRTOS\Source\list.c
-.\objects\list.o: ..\FreeRTOS\Source\Include\list.h
-.\objects\list.o: ..\FreeRTOS\Source\Include\portmacro.h
-.\objects\list.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stdint.h
-.\objects\list.o: C:\Keil_v5\ARM\ARMCC\Bin\..\include\stddef.h
-.\objects\list.o: ..\FreeRTOS\Source\Include\FreeRTOSConfig.h
