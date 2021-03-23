f2dd819fe5261fd78434a93e0ab15c8fdb8fbbe1
csy <csydlut@163.com>
2021-03-23 10:39:56 +0800

    Re-code MyMux2_1 in FPGA.

---
+++ FPGA/YeHuo-Altera/10_MyMux2_1/rtl/mymux2_1.v
@@ -0,0 +1,15 @@
+
+module mymux2_1 (
+    input  wire in1,
+    input  wire in2,
+    input  wire sel,
+
+    output reg  out
+);
+    always @(*) begin
+        if( sel == 1'b1 )
+            out = in1;
+        else
+            out = in2;
+    end
+endmodule
